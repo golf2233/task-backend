77572c1ba4888c73ce7d5b1bba2b547215ea5db8
Gobinath R Muthusamy <gobi@MacBookAir.fritz.box>
2026-01-02 14:32:18 +0100

    Deploy-ready backend

---
+++ models/Task.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
